21a907654beb78d48c76ed8eade80ac678af8465
turzeno <philipp.rueegg@bwz.rappi.ch>
2021-01-25 10:50:44 +0100

    finished circles and animation

--- script.js
+++ script.js
@@ -77,7 +77,12 @@ function gameOver() {
 }
 
 function shapeShooter() {
-
+    const form1 = [];
+    let r = Math.floor(Math.random() * 30) + 15;
+    let x = Math.random() * (canvas.width - r * 2) + r;
+    let y = Math.random() * (canvas.height - r * 2) + r;
+    let c = rgb(68, 68, 65);
+    form1.push(new Circle(x, y, r, c));
 }
 
 function Circle(x, y, r, c) {
@@ -86,9 +91,13 @@ function Circle(x, y, r, c) {
     this.r = r;
     this.c = c;
 
-    //Because of the moving
-    this.dx;
-    this.dy;
+    //Gibt Richtung
+    this.dx = (Math.random() * 4) + 1;
+    //Da man mehrere Richtungen will
+    this.dx = Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+
+    this.dy = (Math.random() * 4) + 1;
+    this.dy = Math.floor(Math.random() * 2) == 1 ? 1 : -1;
 
     this.drawn = function() {
         ctx.beginPath();
@@ -96,7 +105,35 @@ function Circle(x, y, r, c) {
         ctx.arc(this.x, this.y, this.r, 0, Math.Pi * 2);
         ctx.fill();
     }
+
+    this.animate = function() {
+        this.x += this.dx;
+        this.y += this.dy;
+
+        //Damit sie von den Wänden abprallen
+        if (this.x + this.r > canvas.width || this.x - this.r < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.r > canvas.height || this.y - this.r < 0) {
+            this.dy = -this.dy;
+        }
+
+        this.drawn();
+    }
 }
+
+
+
+function Update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < form1.length; i++) {
+        form1[i].animate();
+    }
+
+    requestAnimationFrame(Update);
+}
+Update();
 
 function Rect() {
 
